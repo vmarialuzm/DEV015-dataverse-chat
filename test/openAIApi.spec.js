e13ea4e4fb7d72a54d0d86a114f49db2ecbb51c2
vmarialuzm <vmarialuzm@gmail.com>
2024-10-10 17:07:43 -0500

    iniciando test asincronos

---
+++ test/openAIApi.spec.js
@@ -0,0 +1,8 @@
+import { communicateWithOpenAI } from '../src/lib/openAIApi.js'
+
+describe('getApiKey', () => {
+
+  it('debería devolver la respuesta de openAI', async() => {
+    await expect(communicateWithOpenAI('Hola', 'Machu Picchu').resolves.toBe('¡Hola! ¿En qué puedo ayudarte hoy?'))
+  });
+});
